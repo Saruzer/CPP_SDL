67201535c4e6a8ff1e996f218c120398b999b1bf
Saruzer Saruzer <mrlexa130@gmail.com>
2024-11-30 16:00:57 +0200

    Mouse event

--- CPP_SDL.cpp
+++ CPP_SDL.cpp
@@ -6,21 +6,37 @@ int main(int argc, char* argv[])
 	int w = 640; // window width
 	int h = 480; // window height
 
-	int rs = 4; //Render scale(x,y) size
+	int rs = 10; //Render scale(x,y) size
 	
+	int x = w / (rs * 2); // pixel x pos
+	int y = h / (rs * 2); // pixel y pos
+
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;	
+	SDL_Event e;
+	bool running = true;
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(w, h, 0, &window, &renderer);
-	SDL_RenderSetScale(renderer,rs,rs);
-	
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
+	SDL_RenderSetScale(renderer, rs, rs);
+	while (running) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_EventType::SDL_QUIT) {
+				running = false;
+			}
+			else if (e.type == SDL_EventType::SDL_MOUSEMOTION) {
+				x = e.motion.x;
+				y = e.motion.y;
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderDrawPoint(renderer, w / (rs * 2), h / (rs * 2));
-	SDL_RenderPresent(renderer);
-	SDL_Delay(2000);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderDrawPoint(renderer, x / rs, y / rs);
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(10);
+	}
 	return 1;
 }
