5b3f07b4c1c83719b92a6c34ae6c525edca212b4
Saruzer Saruzer <mrlexa130@gmail.com>
2024-11-30 15:30:29 +0200

    Draw a pixel

--- CPP_SDL.cpp
+++ CPP_SDL.cpp
@@ -3,6 +3,24 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	int w = 640; // window width
+	int h = 480; // window height
+
+	int rs = 4; //Render scale(x,y) size
+	
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;	
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(w, h, 0, &window, &renderer);
+	SDL_RenderSetScale(renderer,rs,rs);
+	
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDrawPoint(renderer, w / (rs * 2), h / (rs * 2));
+	SDL_RenderPresent(renderer);
+	SDL_Delay(2000);
 	return 1;
 }
